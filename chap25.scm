d32dfcacfce7d0d0dd0e9bb3a198de40209e99c9
Josh Simmons <josh.simmons@gmail.com>
2015-08-16 09:34:35 -0700

    Starting on 2.5.3 and removing spaces

--- chap25.scm
+++ chap25.scm
@@ -177,7 +177,7 @@
   ;; internal procedures
   (define (real-part z) (car z))
   (define (imag-part z) (cdr z))
-  (define (make-from-real-imag x y) 
+  (define (make-from-real-imag x y)
     (cons x y))
   (define (magnitude z)
     (sqrt (+ (square (real-part z))
@@ -187,17 +187,17 @@
   (define (make-from-mag-ang r a)
     (cons (* r (cos a)) (* r (sin a))))
   ;; interface to the rest of the system
-  (define (tag x) 
+  (define (tag x)
     (attach-tag 'rectangular x))
   (put 'real-part '(rectangular) real-part)
   (put 'imag-part '(rectangular) imag-part)
   (put 'magnitude '(rectangular) magnitude)
   (put 'angle '(rectangular) angle)
   (put 'make-from-real-imag 'rectangular
-       (lambda (x y) 
+       (lambda (x y)
          (tag (make-from-real-imag x y))))
   (put 'make-from-mag-ang 'rectangular
-       (lambda (r a) 
+       (lambda (r a)
          (tag (make-from-mag-ang r a))))
   'done)
 (install-rectangular-package)
@@ -221,58 +221,58 @@
   (put 'magnitude '(polar) magnitude)
   (put 'angle '(polar) angle)
   (put 'make-from-real-imag 'polar
-       (lambda (x y) 
+       (lambda (x y)
          (tag (make-from-real-imag x y))))
   (put 'make-from-mag-ang 'polar
-       (lambda (r a) 
+       (lambda (r a)
          (tag (make-from-mag-ang r a))))
   'done)
 (install-polar-package)
 
 (define (make-from-real-imag x y)
-  ((get 'make-from-real-imag 
-        'rectangular) 
+  ((get 'make-from-real-imag
+        'rectangular)
    x y))
 
 (define (make-from-mag-ang r a)
-  ((get 'make-from-mag-ang 
-        'polar) 
+  ((get 'make-from-mag-ang
+        'polar)
    r a))
 
-(define (real-part z) 
+(define (real-part z)
   (apply-generic 'real-part z))
-(define (imag-part z) 
+(define (imag-part z)
   (apply-generic 'imag-part z))
-(define (magnitude z) 
+(define (magnitude z)
   (apply-generic 'magnitude z))
-(define (angle z) 
+(define (angle z)
   (apply-generic 'angle z))
 
 (define (install-complex-package)
-  ;; imported procedures from rectangular 
+  ;; imported procedures from rectangular
   ;; and polar packages
   (define (make-from-real-imag x y)
-    ((get 'make-from-real-imag 
-          'rectangular) 
+    ((get 'make-from-real-imag
+          'rectangular)
      x y))
   (define (make-from-mag-ang r a)
-    ((get 'make-from-mag-ang 'polar) 
+    ((get 'make-from-mag-ang 'polar)
      r a))
   ;; internal procedures
   (define (add-complex z1 z2)
-    (make-from-real-imag 
+    (make-from-real-imag
      (+ (real-part z1) (real-part z2))
      (+ (imag-part z1) (imag-part z2))))
   (define (sub-complex z1 z2)
-    (make-from-real-imag 
+    (make-from-real-imag
      (- (real-part z1) (real-part z2))
      (- (imag-part z1) (imag-part z2))))
   (define (mul-complex z1 z2)
-    (make-from-mag-ang 
+    (make-from-mag-ang
      (* (magnitude z1) (magnitude z2))
      (+ (angle z1) (angle z2))))
   (define (div-complex z1 z2)
-    (make-from-mag-ang 
+    (make-from-mag-ang
      (/ (magnitude z1) (magnitude z2))
      (- (angle z1) (angle z2))))
   (define (equ?-complex z1 z2)
@@ -285,25 +285,25 @@
   ;; interface to rest of the system
   (define (tag z) (attach-tag 'complex z))
   (put 'add '(complex complex)
-       (lambda (z1 z2) 
+       (lambda (z1 z2)
          (tag (add-complex z1 z2))))
   (put 'sub '(complex complex)
-       (lambda (z1 z2) 
+       (lambda (z1 z2)
          (tag (sub-complex z1 z2))))
   (put 'mul '(complex complex)
-       (lambda (z1 z2) 
+       (lambda (z1 z2)
          (tag (mul-complex z1 z2))))
   (put 'div '(complex complex)
-       (lambda (z1 z2) 
+       (lambda (z1 z2)
          (tag (div-complex z1 z2))))
   (put 'equ? '(complex complex)
        (lambda (z1 z2)
          (equ?-complex z1 z2)))
   (put 'make-from-real-imag 'complex
-       (lambda (x y) 
+       (lambda (x y)
          (tag (make-from-real-imag x y))))
   (put 'make-from-mag-ang 'complex
-       (lambda (r a) 
+       (lambda (r a)
          (tag (make-from-mag-ang r a))))
   (put 'real-part '(complex) real-part)
   (put 'imag-part '(complex) imag-part)
@@ -340,17 +340,17 @@
                     (a1 (car args))
                     (a2 (cadr args)))
                 (if (not (= type1 type2))
-                  (let ((t1->t2 
+                  (let ((t1->t2
                          (get-coercion type1
                                        type2))
-                        (t2->t1 
-                         (get-coercion type2 
+                        (t2->t1
+                         (get-coercion type2
                                        type1)))
                     (cond (t1->t2
-                           (apply-generic 
+                           (apply-generic
                             op (t1->t2 a1) a2))
                           (t2->t1
-                           (apply-generic 
+                           (apply-generic
                             op a1 (t2->t1 a2)))
                           (else
                            (error "No method for these types"
@@ -402,7 +402,7 @@
                                       (length args)))))
             (if proc
               (apply proc
-                     (map contents 
+                     (map contents
                           (apply-coercions coercions args)))
               (try-coercions type-tags (cdr to-try))))
           (try-coercions type-tags (cdr to-try))))))
@@ -488,7 +488,7 @@
   ;; internal procedures
   (define (real-part z) (car z))
   (define (imag-part z) (cdr z))
-  (define (make-from-real-imag x y) 
+  (define (make-from-real-imag x y)
     (cons x y))
   (define (magnitude z)
     (square-root (add (square (real-part z))
@@ -498,17 +498,17 @@
   (define (make-from-mag-ang r a)
     (cons (mul r (cosine a)) (mul r (sine a))))
   ;; interface to the rest of the system
-  (define (tag x) 
+  (define (tag x)
     (attach-tag 'rectangular x))
   (put 'real-part '(rectangular) real-part)
   (put 'imag-part '(rectangular) imag-part)
   (put 'magnitude '(rectangular) magnitude)
   (put 'angle '(rectangular) angle)
   (put 'make-from-real-imag 'rectangular
-       (lambda (x y) 
+       (lambda (x y)
          (tag (make-from-real-imag x y))))
   (put 'make-from-mag-ang 'rectangular
-       (lambda (r a) 
+       (lambda (r a)
          (tag (make-from-mag-ang r a))))
   'done)
 (install-rectangular-package)
@@ -533,10 +533,10 @@
   (put 'magnitude '(polar) magnitude)
   (put 'angle '(polar) angle)
   (put 'make-from-real-imag 'polar
-       (lambda (x y) 
+       (lambda (x y)
          (tag (make-from-real-imag x y))))
   (put 'make-from-mag-ang 'polar
-       (lambda (r a) 
+       (lambda (r a)
          (tag (make-from-mag-ang r a))))
   'done)
 (install-polar-package)
@@ -553,3 +553,122 @@
 (define (cosine x) ((get 'cos 'scheme-number) (raise-to x (level 0))))
 (define (arctangent x y) ((get 'atan 'scheme-number) (raise-to x (level 0)) (raise-to y (level 0))))
 (define (square-root x) ((get 'sqrt 'scheme-number) (raise-to x (level 0))))
+
+;2.5.3
+(define (install-polynomial-package)
+  ;; internal procedures
+  ;; representation of poly
+  (define (make-poly variable term-list)
+    (cons variable term-list))
+
+  (define (add-terms L1 L2)
+    (cond ((empty-termlist? L1) L2)
+          ((empty-termlist? L2) L1)
+          (else
+           (let ((t1 (first-term L1))
+                 (t2 (first-term L2)))
+             (cond ((> (order t1) (order t2))
+                    (adjoin-term
+                     t1
+                     (add-terms (rest-terms L1)
+                                L2)))
+                   ((< (order t1) (order t2))
+                    (adjoin-term
+                     t2
+                     (add-terms
+                      L1
+                      (rest-terms L2))))
+                   (else
+                    (adjoin-term
+                     (make-term
+                      (order t1)
+                      (add (coeff t1)
+                           (coeff t2)))
+                     (add-terms
+                      (rest-terms L1)
+                      (rest-terms L2)))))))))
+
+  (define (add-poly p1 p2)
+    (if (same-variable? (variable p1)
+                        (variable p2))
+        (make-poly
+         (variable p1)
+         (add-terms (term-list p1)
+                    (term-list p2)))
+        (error "Polys not in same var:
+                ADD-POLY"
+               (list p1 p2))))
+
+  (define (mul-terms L1 L2)
+    (if (empty-termlist? L1)
+        (the-empty-termlist)
+        (add-terms
+         (mul-term-by-all-terms
+          (first-term L1) L2)
+         (mul-terms (rest-terms L1) L2))))
+
+  (define (mul-term-by-all-terms t1 L)
+    (if (empty-termlist? L)
+        (the-empty-termlist)
+        (let ((t2 (first-term L)))
+          (adjoin-term
+           (make-term
+            (+ (order t1) (order t2))
+            (mul (coeff t1) (coeff t2)))
+           (mul-term-by-all-terms
+            t1
+            (rest-terms L))))))
+
+  (define (mul-poly p1 p2)
+    (if (same-variable? (variable p1)
+                        (variable p2))
+        (make-poly
+         (variable p1)
+         (mul-terms (term-list p1)
+                    (term-list p2)))
+        (error "Polys not in same var:
+                MUL-POLY"
+               (list p1 p2))))
+
+  ;; interface to rest of the system
+  (define (tag p) (attach-tag 'polynomial p))
+  (put 'add '(polynomial polynomial)
+       (lambda (p1 p2)
+         (tag (add-poly p1 p2))))
+  (put 'mul '(polynomial polynomial)
+       (lambda (p1 p2)
+         (tag (mul-poly p1 p2))))
+  (put '=zero? '(polynomial)
+       (lambda (p)
+         (empty-termlist? (term-list p))))
+  (put 'make 'polynomial
+       (lambda (var terms)
+         (tag (make-poly var terms))))
+  'done)
+(install-polynomial-package)
+
+(define (make-polynomial var terms)
+  ((get 'make 'polynomial) var terms))
+
+(define (variable p) (car p))
+(define (term-list p) (cdr p))
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1)
+       (variable? v2)
+       (eq? v1 v2)))
+
+;; representation of terms and term lists
+(define (adjoin-term term term-list)
+  (if (=zero? (coeff term))
+      term-list
+      (cons term term-list)))
+(define (the-empty-termlist) '())
+(define (first-term term-list) (car term-list))
+(define (rest-terms term-list) (cdr term-list))
+(define (empty-termlist? term-list)
+  (null? term-list))
+(define (make-term order coeff)
+  (list order coeff))
+(define (order term) (car term))
+(define (coeff term) (cadr term))
